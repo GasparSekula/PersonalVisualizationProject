50c440a77166abad1567cf3e6f2b143fa8c69da0
Michał Piechota <michal@MacBook-Air-Michal.local>
2024-01-10 22:43:05 +0100

    wczytanie i zmergowanie ramek

--- data_import.R
+++ data_import.R
@@ -27,23 +27,13 @@ water_1_df <- water_1_df %>%
 
 ### GENTLEMAN 2
 
-#water_2_df <- read.csv("data/2/water.csv",
-#                       header = TRUE, skip = 2, check.names = FALSE, stringsAsFactors = FALSE)
+#woda spisywana recznie
 
-#colnames(water_2_df) <- c("start_time", "amount", "custom", "update_time", 
-#                          "create_time", "time_offset", "deviceuuid", 
-#                          "unit_amount", "comment", "pkg_name", "datauuid")
+water_2_df <- read.csv("data/2/water.csv",
+                       header = FALSE, check.names = FALSE, stringsAsFactors = FALSE)
 
-# water_2_df <- water_2_df %>% 
-#   select("start_time", "amount") %>% 
-#   mutate(time = ymd_hms(start_time),
-#          day = day(time),
-#          weekday = wday(time, label = TRUE, abbr = FALSE),
-#          hour = hour(time),
-#          dayHour = paste(day, hour, sep = "h"),
-#          date = format(time, "%Y.%m.%d"),
-#          name = "Gentleman2"
-#   )
+colnames(water_2_df) <- c("date", "weekday", "name", "amount")
+
 
 ### GENTLEMAN 3 
 
@@ -102,7 +92,35 @@ step_1_df <- step_1_df %>% select("run_step", "walk_step", "start_time", "count"
          date = format(time, "%Y.%m.%d"),
          name = "Gentleman1")
 
+### GENTLEMAN 2
 
+step_2_df <- read.csv("data/2/steps.csv",
+                      header = TRUE, skip = 2, check.names = FALSE, stringsAsFactors = FALSE)
+
+
+colnames(step_2_df) <-  sub("^com.samsung.health.step_count\\.", "", c("duration", "version_code", "run_step", "walk_step", 
+                                                                       "com.samsung.health.step_count.start_time", 
+                                                                       "com.samsung.health.step_count.sample_position_type", 
+                                                                       "com.samsung.health.step_count.custom", 
+                                                                       "com.samsung.health.step_count.update_time", 
+                                                                       "com.samsung.health.step_count.create_time", 
+                                                                       "com.samsung.health.step_count.count", 
+                                                                       "com.samsung.health.step_count.speed", 
+                                                                       "com.samsung.health.step_count.distance", 
+                                                                       "com.samsung.health.step_count.calorie", 
+                                                                       "com.samsung.health.step_count.time_offset", 
+                                                                       "com.samsung.health.step_count.deviceuuid", 
+                                                                       "com.samsung.health.step_count.pkg_name", 
+                                                                       "com.samsung.health.step_count.end_time", 
+                                                                       "com.samsung.health.step_count.datauuid"))
+
+
+step_2_df <- step_2_df %>% select("run_step", "walk_step", "start_time", "count", "speed", "distance") %>% 
+  mutate(time = ymd_hms(start_time),
+         weekday = wday(time, label = TRUE, abbr = FALSE, locale = "en_US"),
+         dayHour = paste(day(time), hour(time), sep = "h"),
+         date = format(time, "%Y.%m.%d"),
+         name = "Gentleman2")
 
 ################################################################################
 ################################################################################
@@ -136,4 +154,11 @@ sleep_1_df <- sleep_1_df %>%
          day = day(end_time),
          weekday = wday(end_time, label = TRUE, abbr = FALSE, locale = "en_US"),
          name = "Gentleman1")
+
+### GENTLEMAN 2
+
+#spisywane recznie
+
+sleep_2_df <- read.csv("data/2/sleep.csv",
+                       header = TRUE, check.names = FALSE, stringsAsFactors = FALSE)
 
